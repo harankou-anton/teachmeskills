8fc4ca55b85b9fa31daee1111d27dba17bd43ef3
Anton Harankou <harankou.anton@gmail.com>
2023-02-11 22:02:34 +0300

    Add homework 13

--- lesson 13/main.py
+++ lesson 13/main.py
@@ -1,9 +1,7 @@
 import logging
-from sqlalchemy import create_engine, and_
+from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists
-from utils import create_tables
-
-from models import User, Profile, Address, Product, Purchase
+import utils
 
 logging.basicConfig(level=logging.INFO)
 loger = logging.getLogger(__name__)
@@ -13,80 +11,13 @@ DB_PASSWORD = "Olimpiec2012!"
 DB_NAME = "les_13"
 
 
-def create_user(session, email, password, phone, age):
-    new_user = User(email=email, password=password)
-    session.add(new_user)
-    session.commit()
-    new_profile = Profile(phone=phone, age=age, user_id=new_user.id)
-    session.add(new_profile)
-    session.commit()
-    return new_user
-
-
-def find_user(session, email):
-    query = session.query(User).filter_by(email=email).first()
-    return query
-
-
-def add_address(session, new_address, new_city, user_id):
-    address = Address(city=new_city, address=new_address, user_id=user_id)
-    session.add(address)
-    session.commit()
-
-
-def update_address(session, old_address, old_city, new_address, new_city, user_id):
-    session.query(Address).filter(and_(Address.address == old_address, Address.city == old_city,
-                                       Address.user_id == user_id)).update({'city': f'{new_city}',
-                                                                            'address': f'{new_address}'})
-    session.commit()
-
-
-def add_product(session, name, price, quantity, comment):
-    new_product = Product(name=name, price=price, quantity=quantity, comment=comment)
-    session.add(new_product)
-    session.commit()
-
-
-def read_products(session):
-    select_all = session.query(Product).select_from(Product).all()
-    return select_all
-
-
-def update_product(session, id_prod, name, price, quantity, comment):
-    session.query(Product).filter_by(id=id_prod).update({"name": f"{name}", "price": f"{price}",
-                                                         "quantity": f"{quantity}", "comment": f"{comment}"})
-    session.commit()
-
-
-def delete_product(session, id_prod):
-    session.query(Product).filter_by(id=id_prod).delete()
-    session.commit()
-
-
-def add_purchase(session, user_id, prod_id, quantity):
-    purchase = Purchase(user_id=user_id, product_id=prod_id, quantity=quantity)
-    session.add(purchase)
-    session.commit()
-
-
-def search_purchase_by_user(session, user_id):
-    query = session.query(Purchase).filter(Purchase.user_id == user_id).all()
-    return query
-
-
 if __name__ == "__main__":
     engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}")
     if not database_exists(engine.url):
         create_database(engine.url)
 
-
-
-    session = create_tables(engine)
-
-    # query = session.query(User).filter_by(id=2).all()
-    # print(query[0].addresses)
-
-
+    session = utils.create_tables(engine)
+#
     menu = """
 1 - Создать пользователя
 2 - Найти пользователя
@@ -97,6 +28,8 @@ if __name__ == "__main__":
 7 - Удалить продукт
 8 - Добавить покупку
 9 - Поиск покупок польльзователя
+10 - Фильтр покупок пользователей по цене
+11 - Поиск пользователей по купленному товару
 0 - Выйти
     """
 
@@ -115,11 +48,11 @@ if __name__ == "__main__":
             password = input('Введите пароль: ')
             age = int(input('Введите возраст: '))
             phone = input('Введите номер телефона: ')
-            user = create_user(session=session, email=email, password=password, phone=phone, age=age)
+            user = utils.create_user(session=session, email=email, password=password, phone=phone, age=age)
             loger.info(f'Пользователь {user.id} создан')
         elif choice == '2':
             email = input('Введите email пользователя: ')
-            get_user = find_user(session=session, email=email)
+            get_user = utils.find_user(session=session, email=email)
             if get_user is not None:
                 loger.info(f'id: {get_user.id}, email: {get_user.email}, password: {get_user.password}, '
                            f'age: {get_user.profile.age}, phone: {get_user.profile.phone}')
@@ -129,23 +62,23 @@ if __name__ == "__main__":
             city = input('Введите город: ')
             address = input('Введите адрес: ')
             email = input('Введите email пользователя: ')
-            get_user = find_user(session=session, email=email)
+            get_user = utils.find_user(session=session, email=email)
             if get_user is None:
                 loger.info(f'Пользователя с email - {email} не существует.')
             elif len(get_user.addresses) == 0:
-                add_address(session=session, new_city=city, new_address=address, user_id=get_user.id)
+                utils.add_address(session=session, new_city=city, new_address=address, user_id=get_user.id)
                 loger.info(f'Адрес для пользователя {email} добавлен.')
             else:
                 loger.info(address_action)
                 action = input('Выберите действие: ')
                 if action == '1':
-                    add_address(session=session, new_city=city, new_address=address, user_id=get_user.id)
+                    utils.add_address(session=session, new_city=city, new_address=address, user_id=get_user.id)
                     loger.info(f'Адрес для пользователя {email} добавлен.')
                 elif action == '2':
                     if len(get_user.addresses) == 1:
-                        update_address(session=session, old_address=get_user.addresses[0].address,
-                                       old_city=get_user.addresses[0].city, new_city=city, new_address=address,
-                                       user_id=get_user.id)
+                        utils.update_address(session=session, old_address=get_user.addresses[0].address,
+                                             old_city=get_user.addresses[0].city, new_city=city, new_address=address,
+                                             user_id=get_user.id)
                         loger.info(f'Адрес для пользователя {email} обновлен.')
                     else:
                         all_addresses = ''''''
@@ -153,22 +86,23 @@ if __name__ == "__main__":
                             all_addresses += f'{pos} address: {addr.address}, city: {addr.city}\n'
                         loger.info(f'\n{all_addresses}')
                         choice_addr = int(input('Выберите адрес для обновления: '))
-                        update_address(session=session, old_address=get_user.addresses[choice_addr].address,
-                                       old_city=get_user.addresses[choice_addr].city, new_city=city,
-                                       new_address=address, user_id=get_user.id)
+                        utils.update_address(session=session, old_address=get_user.addresses[choice_addr].address,
+                                             old_city=get_user.addresses[choice_addr].city, new_city=city,
+                                             new_address=address, user_id=get_user.id)
                         loger.info(f'Адрес для пользователя {email} обновлен.')
         elif choice == '4':
             name = input('Название продукта: ')
             price = input('Цена продутка: ')
             quantity = input('Количество продукта: ')
             comment = input('Комментарий: ')
-            add_product(session=session, name=name, price=price, quantity=quantity, comment=comment)
+            utils.add_product(session=session, name=name, price=price, quantity=quantity, comment=comment)
             loger.info('Новый продукт добавлен.')
         elif choice == '5':
             all_products = ''''''
-            list_prods = read_products(session=session)
+            list_prods = utils.read_products(session=session)
             for el in list_prods:
-                item = f'ID: {el.id}, Name: {el.name}, Quantity: {el.quantity}, Price: {el.price}, Comment: {el.comment}'
+                item = f'ID: {el.id}, Name: {el.name}, Quantity: {el.quantity}, Price: {el.price}, ' \
+                       f'Comment: {el.comment}'
                 all_products += f'{item}\n'
             loger.info(f'\n{all_products}')
         elif choice == '6':
@@ -177,32 +111,35 @@ if __name__ == "__main__":
             price = input('Цена продутка: ')
             quantity = input('Количество продукта: ')
             comment = input('Комментарий: ')
-            update_product(session=session, id_prod=id_prod, name=name, price=price, quantity=quantity, comment=comment)
+            utils.update_product(session=session, id_prod=id_prod, name=name, price=price, quantity=quantity,
+                                 comment=comment)
             loger.info(f'Продукт ID={id_prod} обновлен')
         elif choice == '7':
             id_prod = int(input('ID продукта: '))
-            delete_product(session=session, id_prod=id_prod)
+            utils.delete_product(session=session, id_prod=id_prod)
             loger.info(f'Продукт ID={id_prod} удалён.')
         elif choice == '8':
             id_prod = int(input('ID продукта: '))
             id_user = int(input('ID пользователя: '))
             quantity = int(input('Количество: '))
-            add_purchase(session=session, user_id=id_user, prod_id=id_prod, quantity=quantity)
+            utils.add_purchase(session=session, user_id=id_user, prod_id=id_prod, quantity=quantity)
             loger.info('Покупка добавлена.')
         elif choice == '9':
             id_user = int(input('ID пользователя: '))
-            search_purchase_by_user(session=session, user_id=id_user)
+            utils.search_purchase_by_user(session=session, user_id=id_user)
             all_purchase = ''''''
-            list_purchase  = search_purchase_by_user(session=session, user_id=id_user)
+            list_purchase = utils.search_purchase_by_user(session=session, user_id=id_user)
             for el in list_purchase:
-                item = f'ID: {el.id}, Name: {el.name}, Quantity: {el.quantity}, Price: {el.price}, Comment: {el.comment}'
+                item = f'Name: {el.product.name}, Quantity: {el.quantity}'
                 all_purchase += f'{item}\n'
             loger.info(f'\n{all_purchase}')
-
-
-
-
-
-
-
-
+        elif choice == '10':
+            price_more = int(input('Введите цену для фильтра: '))
+            result = utils.search_users_by_purchase_price(session, price_more)
+            for item in result:
+                loger.info(f'User email: {item[0]}, Product name: {item[2]}, Price: {item[1]}')
+        elif choice == '11':
+            product_name = input('Введите название продукта: ')
+            result = utils.search_users_by_purchase_product(session, product_name)
+            for item in result:
+                loger.info(f'User: {item[0]}')
--- lesson 13/utils.py
+++ lesson 13/utils.py
@@ -1,8 +1,83 @@
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_
 from models import Base
+from models import User, Profile, Address, Product, Purchase
 
 
 def create_tables(engine):
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     return Session()
+
+
+def create_user(session, email: str, password: str, phone: str, age: int) -> User:
+    new_user = User(email=email, password=password)
+    session.add(new_user)
+    session.commit()
+    new_profile = Profile(phone=phone, age=age, user_id=new_user.id)
+    session.add(new_profile)
+    session.commit()
+    return new_user
+
+
+def find_user(session, email: str):
+    query = session.query(User).filter_by(email=email).first()
+    return query
+
+
+def add_address(session, new_address: str, new_city: str, user_id: int):
+    address = Address(city=new_city, address=new_address, user_id=user_id)
+    session.add(address)
+    session.commit()
+
+
+def update_address(session, old_address: str, old_city: str, new_address: str, new_city: str, user_id: int):
+    session.query(Address).filter(and_(Address.address == old_address, Address.city == old_city,
+                                       Address.user_id == user_id)).update({'city': f'{new_city}',
+                                                                            'address': f'{new_address}'})
+    session.commit()
+
+
+def add_product(session, name: str, price: float, quantity: int, comment: str):
+    new_product = Product(name=name, price=price, quantity=quantity, comment=comment)
+    session.add(new_product)
+    session.commit()
+
+
+def read_products(session) -> list:
+    select_all = session.query(Product).select_from(Product).all()
+    return select_all
+
+
+def update_product(session, id_prod: int, name: str, price: float, quantity: int, comment: str):
+    session.query(Product).filter_by(id=id_prod).update({"name": f"{name}", "price": f"{price}",
+                                                         "quantity": f"{quantity}", "comment": f"{comment}"})
+    session.commit()
+
+
+def delete_product(session, id_prod: int):
+    session.query(Product).filter_by(id=id_prod).delete()
+    session.commit()
+
+
+def add_purchase(session, user_id: int, prod_id: int, quantity: int):
+    purchase = Purchase(user_id=user_id, product_id=prod_id, quantity=quantity)
+    session.add(purchase)
+    session.commit()
+
+
+def search_purchase_by_user(session, user_id: int) -> list:
+    query = session.query(Purchase).filter(Purchase.user_id == user_id).all()
+    return query
+
+
+def search_users_by_purchase_price(session, price:float) -> list:
+    query = session.query(User.email, Product.price * Purchase.quantity, Product.name).select_from(User) \
+        .join(Purchase).join(Product).where((Product.price * Purchase.quantity) > price).all()
+    return query
+
+
+def search_users_by_purchase_product(session, product_name: str) -> list:
+    query = session.query(User.email).distinct(User.email)\
+        .join(Purchase).join(Product).filter(Product.name == product_name).all()
+    return query
